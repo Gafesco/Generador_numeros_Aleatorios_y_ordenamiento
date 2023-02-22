a8c0df34e6e47dbab593395fdc76210f8d18358f
Gafesco <110783476+Gafesco@users.noreply.github.com>
2023-02-22 11:36:23 -0600

    Add files via upload

---
+++ ProgramaNumerosAleatorios/src/programanumerosaleatorios/ProgramaNumerosAleatorios.java
@@ -0,0 +1,16 @@
+
+package programanumerosaleatorios;
+
+/**
+ *
+ * @author Gabriel Escobar
+ */
+public class ProgramaNumerosAleatorios {
+
+
+    public static void main(String[] args) {
+frm_numeros frm = new frm_numeros();
+frm.show();
+    }
+    
+}
